f55b26e9c341a22901c705159b1256670b902373
Carlos Duarte <39272982+sukiyanen85@users.noreply.github.com>
2020-05-09 16:15:07 +0100

    chore: create app routes

--- src/TaskManager.js
+++ src/TaskManager.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './TaskManager.css';
+import { useRoutes } from 'hookrouter';
+import TaskList from './List/TaskList';
+import TaskAdd from './Add/TaskAdd';
+import TaskEdit from './Edit/TaskEdit';
+
+const routes = {
+  '/': () => <TaskList />,
+  '/add': () => <TaskAdd />,
+  '/edit:id': ({id}) => <TaskEdit id={id}/>
+}
 
 function TaskManager() {
-  return (
-    <>
-      <h1>Task Manager</h1>
-    </>
-  );
+  return useRoutes(routes);
 }
 
 export default TaskManager;
