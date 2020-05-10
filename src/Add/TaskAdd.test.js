f3ad274b11ce4c2688eacf6b02eb3de5cec468e2
Carlos Duarte <39272982+sukiyanen85@users.noreply.github.com>
2020-05-10 16:11:58 +0100

    chore: task creation test

--- src/Add/TaskAdd.test.js
+++ src/Add/TaskAdd.test.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { render, fireEvent } from '@testing-library/react';
 import TaskAdd from './TaskAdd';
+import '@testing-library/jest-dom/extend-expect';
 
 describe('TaskAdd tests', () => {
-    test('Render TaskAdd component', () => {
+    it('Render TaskAdd component', () => {
         const div = document.createElement('div');
         ReactDOM.render(<TaskAdd />, div);
         ReactDOM.unmountComponentAtNode(div);
     });
+
+    it('Adding new Task', () => {
+        const { getByTestId } = render(<TaskAdd />);
+        fireEvent.change(getByTestId('task'), { target: { value: 'This is a task'}});
+        fireEvent.click(getByTestId('btn-submit'));
+        expect(getByTestId('modal')).toHaveTextContent('Task added successfully');
+    });
 });
+
