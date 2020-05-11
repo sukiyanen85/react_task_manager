8c4df98a2db0f36ff06fc0f546ee2a78831d697e
Carlos Duarte <39272982+sukiyanen85@users.noreply.github.com>
2020-05-11 19:11:11 +0100

    chore: pagination test

--- src/Pagination/TaskPagination.test.js
+++ src/Pagination/TaskPagination.test.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import TaskPagination from './TaskPagination';
 import '@testing-library/jest-dom/extend-expect';
 
 describe('TaskPagination tests', () => {
     it('Render TaskPagination component', () => {
         const div = document.createElement('div');
-        ReactDOM.render(<TaskPagination />, div);
+        ReactDOM.render(<TaskPagination totalTasks={3} tasksPerPage={3} currentPage={1} changePage={() => false} />, div);
         ReactDOM.unmountComponentAtNode(div);
     });
 
-/*     it('Adding new TaskPagination', () => {
-        const { getByTestId } = render(<TaskAdd />);
-        fireEvent.change(getByTestId('task'), { target: { value: 'This is a task'}});
-        fireEvent.click(getByTestId('btn-submit'));
-        expect(getByTestId('modal')).toHaveTextContent('Task added successfully');
-    }); */
+    
+    it('Pagination must show 3 pages', () => {
+        const { getByTestId } = render(<TaskPagination totalTasks={15} tasksPerPage={5} currentPage={1} changePage={() => false} />);
+        const pagination = getByTestId('pagination');
+
+        expect(pagination).toHaveTextContent('1');
+        expect(pagination).toHaveTextContent('2');
+        expect(pagination).toHaveTextContent('3');
+    }); 
 });
