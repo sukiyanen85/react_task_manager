379539ca37808bab5e014b0e1d1eef40087d4487
Carlos Duarte <39272982+sukiyanen85@users.noreply.github.com>
2020-05-12 13:58:24 +0100

    chore: edit tasks

--- src/Edit/TaskEdit.js
+++ src/Edit/TaskEdit.js
@@ -1,13 +1,95 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Jumbotron, Form, Button, Modal } from 'react-bootstrap';
+import { A, navigate } from 'hookrouter';
+import PropTypes from 'prop-types';
 
-function TaskEdit() {
-    /* const [, setFunction] = useState(false); */
+function TaskEdit(props) {
+    const [task, setTask] = useState('');
+    const [taskIsLoaded, setTaskIsLoaded] = useState(false);
+    const [showModal, setShowModal] = useState(false); 
+    const [formValidated, setFormValidated] = useState(false); 
+
+    useEffect(() => {
+        function loadTask(){
+            const tasks = JSON.parse(localStorage['tasks']);
+            const task = tasks.filter(task => {
+                return task.id === parseInt(props.id)
+            });
+
+            setTask(task[0].name);
+        }
+    
+        if(! taskIsLoaded){
+            loadTask();
+            setTaskIsLoaded(true);
+        }
+    }, [taskIsLoaded, props]);
+
+    function closeModal(){
+        setShowModal(false);
+        navigate('/');
+    }
+
+    function handleTask(event){
+        setTask(event.target.value);
+    }
+
+    function formSubmited(event) {
+        event.preventDefault();
+        setFormValidated(true);
+    
+        if (event.currentTarget.checkValidity() === true) { // If the form is validated
+            const tasks = JSON.parse(localStorage['tasks']);
+            tasks.map(helperTask => {
+                if(helperTask.id === parseInt(props.id)){
+                    helperTask.name = task;
+                }
+
+                return helperTask;
+            });
+
+            localStorage['tasks'] = JSON.stringify(tasks);
+            setShowModal(true);
+        }
+    }
 
     return (
         <>
-            <h1>Task Edit Component</h1>
+            <h3 className="text-center">Edit Task</h3>
+
+            <Jumbotron>
+                <Form onSubmit={formSubmited} noValidate validated={formValidated}>
+                    <Form.Group>
+                        <Form.Label>Task Name</Form.Label>
+                        <Form.Control type="text" placeholder="Name" minLength="5" maxLength="15" required value={task} onChange={handleTask} data-testid="task" />
+                        <Form.Control.Feedback type="invalid">
+                            This field must contain between 5 and 15 characters
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group className="text-center">
+                        <Button type="submit" variant="success" data-testid="btn-submit">Update</Button>{' '}
+                        <A href="/" className="btn btn-light">Cancel</A>
+                    </Form.Group>
+                </Form>
+            </Jumbotron>
+
+            <Modal show={showModal} onHide={closeModal} data-testid="modal">
+                <Modal.Header closeButton>
+                  <Modal.Title>Success!</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Your Task was succesfully updated!</Modal.Body>
+                <Modal.Footer>
+                  <Button variant="success" onClick={closeModal}>
+                    Close
+                  </Button>
+                </Modal.Footer>
+            </Modal>
         </>
     );
+}
+
+TaskEdit.propTypes = {
+    id: PropTypes.string.isRequired,
 }
 
 export default TaskEdit;
--- src/Edit/TaskEdit.test.js
+++ src/Edit/TaskEdit.test.js
@@ -2,11 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { render, fireEvent } from '@testing-library/react';
 import TaskEdit from './TaskEdit';
+import Task from '../models/task.model';
+import '@testing-library/jest-dom/extend-expect';
 
 describe('TaskEdit tests', () => {
-    test('Render TaskEdit component', () => {
+
+    beforeEach(() => {
+        localStorage['tasks'] = JSON.stringify([ new Task(1, 'Testing', false)])
+    });
+
+    it('Render TaskEdit component', () => {
         const div = document.createElement('div');
-        ReactDOM.render(<TaskEdit id={1} />, div);
+        ReactDOM.render(<TaskEdit id={'1'} />, div);
         ReactDOM.unmountComponentAtNode(div);
+    });
+
+    it('Show Modal when update', () => {
+        const { getByTestId } = render(<TaskEdit id={'1'} />);
+        fireEvent.change(getByTestId('task'), { target: { value: 'New Task'}});
+        fireEvent.click(getByTestId('btn-submit'));
+        expect(getByTestId('modal')).toHaveTextContent('Your Task was succesfully updated!');
+    });
+
+    it('Update task', () => {
+        const { getByTestId } = render(<TaskEdit id={'1'} />);
+        fireEvent.change(getByTestId('task'), { target: { value: 'New Task'}});
+        fireEvent.click(getByTestId('btn-submit'));
+        
+        const tasks = JSON.parse(localStorage['tasks']);
+        expect(tasks[0].name).toEqual('New Task');
     });
 });
